81c1bc0523f28a6af71d6443124c5c65d6b68fc7
Matthew Brien <matthewbrien@outlook.com>
2015-10-17 18:11:15 -0400

    added home page, and html form

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -12,9 +15,14 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+app.get('/home', function(request, response){
+  response.render('pages/home');
+});
+
+app.get('/HelloWorld', function(request, response){
+  response.end("HelloWorld, how do you like them apples?");
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
