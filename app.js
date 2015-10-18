b6c9cb7a076b2c2e49ba839b3f26e9edc8fa7357
Aaron Miller <aaroneusthegreat@yahoo.com>
2015-10-17 22:30:51 -0400

    Super awesome commit and shit

--- app.js
+++ app.js
@@ -56,17 +56,4 @@ app.use(function(err, req, res, next) {
   });
 });
 
-var MongoClient = require('mongodb').MongoClient
-    , format = require('util').format;
-MongoClient.connect('mongodb://heroku_7w45jd6n:9uu5gh7nlpqkg8h15girn5k90e@ds039404.mongolab.com:39404/heroku_7w45jd6n', function (err, db) {
-    if (err) {
-        throw err;
-    } else {
-        console.log("successfully connected to the database");
-    }
-    db.close();
-});
-
-
-
 module.exports = app;
