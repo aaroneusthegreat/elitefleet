47af08ff2727a5c338dbf2ba761e7caf7a8ae4c6
Aaron Miller <aaroneusthegreat@yahoo.com>
2015-10-17 23:00:05 -0400

    View test

---
+++ views/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(request, response){
+  response.render('index');
+});
+
+app.get('/HelloWorld', function(request, response){
+  response.end("HelloWorld, how do you like them apples?");
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
