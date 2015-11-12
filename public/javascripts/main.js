fc153d5df1a24b841b943d2a0501b80c3a1d43e5
Aaron Miller <aaroneusthegreat@yahoo.com>
2015-11-11 21:46:21 -0500

    adding easyui

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,17 +1,13 @@
 
 	$(document).ready(function(){
-		var grid = dhtmlXGridFromTable("cars_table");
-		grid.enableAutoHeight(true, 900, 300);
-		//grid.setColMinWidth("200,200,200")
-		grid.setSizes();
-		console.log("TEST")
-		// grid.setHeight(150);
+		// 
 		//$('.nav-block').css('width', '100%');
 		//$('.nav-block').css('max-width', '200px');
 		//$(window).resize(function(){
-		//	var windowWidth = $(window).width();
-		//	$('.cars_table').css("width",(windowWidth * .8)+"px");
+	
 		//});
+		$('#cars_table').css("width","85%")
+		$('#cars_table').find('tr').css("width","100%")
 	})
 
 	
