4dd9a242790fd35f6940bd18ea1fd9c8b8ca6d69
Aaron Miller <aaroneusthegreat@yahoo.com>
2015-11-12 01:06:28 -0500

    Menu added to left panel

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,25 +4,26 @@ $(document).ready(function(){
 	
 	    
 
-    $('#dg').datagrid({
-        columns:[[
-            {field:'make',title:'Make',width:200},
-            {field:'model',title:'Model',width:200},
-            {field:'year',title:'Year',width:200,align:'right'}
-        ]]
-    });
+//   $('#dg').datagrid({
+//        columns:[[
+//            {field:'make',title:'Make',width:200},
+//            {field:'model',title:'Model',width:200},
+//            {field:'year',title:'Year',width:200,align:'right'}
+//        ]]
+//   });
 
     
-	$('#dg').datagrid({
-		data: [
-			{make:'easyui', model:'is', year:'awesome'},
-			{make:'easyui', model:'is', year:'awesome'},
-			{make:'easyui', model:'is', year:'awesome'},
-			{make:'easyui', model:'is', year:'awesome'},
-			{make:'easyui', model:'is', year:'awesome'},
-			{make:'easyui', model:'is', year:'awesome'}
-		]
-	});
+//	$('#dg').datagrid({
+//		data: [
+//			{make:'easyui', model:'is', year:'awesome'},
+//			{make:'easyui', model:'is', year:'awesome'},
+//			{make:'easyui', model:'is', year:'awesome'},
+//			{make:'easyui', model:'is', year:'awesome'},
+//			{make:'easyui', model:'is', year:'awesome'},
+//			{make:'easyui', model:'is', year:'awesome'}
+//		]
+//	});
+
 
     $('#dg').datagrid('reload');    // reload the current page data
 
@@ -33,6 +34,15 @@ $(document).ready(function(){
     $('.nav-block').find('td').css("background-repeat","no-repeat")
     $('.nav-block').find('td').css("max-width","300px")
     $('.nav-block').find('td').css("min-width","130px")
+   // $('.nav-block').find('td').css("position","absolute")
+    //$('.nav-block').find('p').css("position","relative")
+    $('.nav-block').find('p').css("width","100%")
+    $('.nav-block').find('p').css("min-height","45px")
+   	$('.nav-block').find('p').css("padding-top","5%")
+    $('.nav-block').find('p').css("text-align","center")
+
+   // console.log($.url().param('vehiclelist'));
+   //table(id="dg" style="width:95%;max-height:600px;padding-top:50px;")
 });
 	
 
