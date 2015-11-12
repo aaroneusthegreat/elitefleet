4459dd3cf0c199f277bd6b35cc9ae2b623fc5feb
Aaron Miller <aaroneusthegreat@yahoo.com>
2015-11-11 19:56:05 -0500

    Got Rid of window for index page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,49 +1,18 @@
 
+	$(document).ready(function(){
+		var grid = dhtmlXGridFromTable("cars_table");
+		grid.enableAutoHeight(true, 900, 300);
+		//grid.setColMinWidth("200,200,200")
+		grid.setSizes();
+		console.log("TEST")
+		// grid.setHeight(150);
+		//$('.nav-block').css('width', '100%');
+		//$('.nav-block').css('max-width', '200px');
+		$(window).resize(function(){
+			var windowWidth = $(window).width();
+			$('.cars_table').width(windowWidth * .8);
+		});
+	})
 
+	
 
-
-
-    var dhxWins
-
-    dhxWins = new dhtmlXWindows();
-    dhxWins.attachViewportTo("winVP");
-
-
-    var idPrefix = 1;
-    function createWindow() {
-        var p = 0;
-        dhxWins.forEachWindow(function(){p++;});
-        if (p>5) {
-            alert("Too many windows");
-            return;
-        }
-        var id = "userWin"+(idPrefix++);
-        //
-        var w = 1245;
-        var h = 420;
-        var x = 0;
-        var y = 0;
-        //
-        dhxWins.createWindow("main_window", x, y, w, h);
-      //  dhxWins.window(id).setText(document.getElementById("win_t").value);
-        // dhxWins.window(id).keepInViewport(true);
-        //
-    //    document.getElementById("win_x").value = x+8;
-      //  document.getElementById("win_y").value = y+6;
-    }
-
-    function doOnUnload() {
-        if (dhxWins != null && dhxWins.unload != null) {
-            dhxWins.unload();
-            dhxWins = null;
-        }
-    }
-
-    createWindow();
-
-    dhxWins.window("main_window").attachURL("partials/index_window.html");
-
-
-
-
-   //$('h1').css('color','red');
